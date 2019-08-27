c67da0956fd2113e20c6d9c19f0a2e90678ab983
Kevin Mijares <50600961+Joseph675@users.noreply.github.com>
2019-08-27 12:54:17 -0500

    alv :v:

--- resources/js/SincronizacionCtrl.js
+++ resources/js/SincronizacionCtrl.js
@@ -3,15 +3,13 @@ angular.module('todo')
 .controller('SincronizacionCtrl', function($scope, ConexionServ, $filter){
 	ConexionServ.createTables();
 	
-	
-
-	
 
 	$scope.mostrarusuarios = function(result){
-		consulta = "SELECT *, rowid FROM usuarios"
+		consulta = "SELECT *, rowid FROM usuarios WHERE id is NULL or eliminado=1 or modificado=1"
 		ConexionServ.query(consulta, []).then(function(result){
 			$scope.usuarios = result;
 			console.log('usuario mostrado')
+			console.log(result)
 
 		}, function(tx){
 			console.log('usuarios no mostrado', tx)
